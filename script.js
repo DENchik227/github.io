acde0145f9f806f1f7b97c6649abf8bf23cc90fd
DENchik227 <denysrik83@gmail.com>
2024-11-26 21:10:11 +0100

    fixing media rules and cosmetic improvements

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-
 function sendEmail() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-  
-    // Создание шаблона mailto-ссылки
-    const subject = encodeURIComponent(`Новое сообщение от ${name}`);
-    const body = encodeURIComponent(`Имя: ${name}\nEmail: ${email}\nСообщение:\n${message}`);
-    
-    window.location.href = `mailto:your-email@example.com?subject=${subject}&body=${body}`;
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  if (!name || !email || !message) {
+      alert("Please fill in all fields.");
+      return;
   }
+
+  const subject = encodeURIComponent(`New message from ${email}`);
+  const body = encodeURIComponent(`${message}`);
+  window.location.href = `mailto:denysrik83@gmail.com?subject=${subject}&body=${body}`;
+}
